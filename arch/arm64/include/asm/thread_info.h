78a4618a6c7a76ece3214d76cd50f21903ba1771
Will Deacon <will.deacon@arm.com>
2018-09-20 10:26:40 +0100

    arm64: preempt: Provide our own implementation of asm/preempt.h
    The asm-generic/preempt.h implementation doesn't make use of the
    PREEMPT_NEED_RESCHED flag, since this can interact badly with load/store
    architectures which rely on the preempt_count word being unchanged across
    an interrupt.
    
    However, since we're a 64-bit architecture and the preempt count is
    only 32 bits wide, we can simply pack it next to the resched flag and
    load the whole thing in one go, so that a dec-and-test operation doesn't
    need to load twice.
    
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -47,6 +47,18 @@ struct thread_info {
 #ifdef CONFIG_SHADOW_CALL_STACK
 	void			*shadow_call_stack;
 #endif
+	union {
+		u64		preempt_count;	/* 0 => preemptible, <0 => bug */
+		struct {
+#ifdef CONFIG_CPU_BIG_ENDIAN
+			u32	need_resched;
+			u32	count;
+#else
+			u32	count;
+			u32	need_resched;
+#endif
+		} preempt;
+	};
 };
 
 #define INIT_THREAD_INFO(tsk)						\
