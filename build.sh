1893bd4063afa9eaf6fc58e3737fd7de4d049ce7
Tetsuo Osaka <tetsugit@gmail.com>
2022-04-13 18:48:25 +0200

    add build sh

---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# Copyright (C) 2020-2021 Adithya R.
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="Origin-X3-S-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$HOME/proton-clang"
+DEFCONFIG="raphael_defconfig"
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+	echo "Atom-X clang not found! Cloning to $TC_DIR..."
+	if ! git clone --depth=1 https://github.com/kdrag0n/proton-clang.git "$TC_DIR"; then
+		echo "Cloning failed! Aborting..."
+		exit 1
+	fi
+fi
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	exit
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+	rm -rf out
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz dtbo.img
+
+kernel="out/arch/arm64/boot/Image.gz"
+dtb="out/arch/arm64/boot/dts/qcom/sm8150-v2.dtb"
+dtbo="out/arch/arm64/boot/dtbo.img"
+
+if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	if ! git clone -q https://github.com/Patriot-06/AnyKernel3 -b x3; then
+		echo -e "\nCloning AnyKernel3 repo failed! Aborting..."
+		exit 1
+	fi
+	cp $kernel $dtbo AnyKernel3
+	cp $dtb AnyKernel3/dtb
+	rm -f *zip
+	cd AnyKernel3 || exit
+	rm -rf out/arch/arm64/boot
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo "Zip: $ZIPNAME"
+	gdrive upload --share "$ZIPNAME"
+	curl --upload-file "$ZIPNAME" https://free.keep.sh
+	echo
+else
+	echo -e "\nCompilation failed!"
+fi
+
+
