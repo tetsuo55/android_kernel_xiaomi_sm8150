7118d7f989bea38bdce713e28bed4e5936c8ae11
Stephen Xian <stevexian56@gmail.com>
2022-06-06 14:09:56 +0800

    drm: Add xiaomi's dynamic fps
    Signed-off-by: Stephen Xian <stevexian56@gmail.com>

--- drivers/gpu/drm/drm_internal.h
+++ drivers/gpu/drm/drm_internal.h
@@ -187,6 +187,10 @@ ssize_t dsi_display_mipi_reg_write(struct drm_connector *connector,
 								 char *buf, size_t count);
 ssize_t dsi_display_mipi_reg_read(struct drm_connector *connector,
 								 char *buf);
+
+ssize_t dsi_display_dynamic_fps_read(struct drm_connector *connector,
+				char *buf);
+
 //thermal_hbm_disabled
 int dsi_display_get_thermal_hbm_disabled(struct drm_connector *connector,
 								bool *thermal_hbm_disabled);
