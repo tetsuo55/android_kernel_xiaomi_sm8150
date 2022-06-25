d5d2acc9b82ec7b61a4c15b9b151523b90fb2147
Tetsuo Osaka <tetsugit@gmail.com>
2022-06-25 16:54:48 +0200

    hacks n shit

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2251,7 +2251,7 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 		return -EINVAL;
 
 	  if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) && (time_before(jiffies, last_input_time + msecs_to_jiffies(3000)))) {
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 		cpu_input_boost_kick();
 
 	}
