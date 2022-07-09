09dd61c27432419572839eda4c7c6b1cc77d01ab
Tetsuo Osaka <tetsugit@gmail.com>
2022-07-09 13:56:50 +0200

    Revert "Revert "qos: Speed up pm_qos_set_value_for_cpus()""
    This reverts commit 3a470c8091f1a8016aae9e5b8e79de03adc7425b.

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -270,12 +270,14 @@ static const struct file_operations pm_qos_debug_fops = {
 
 static inline int pm_qos_set_value_for_cpus(struct pm_qos_request *new_req,
 					    struct pm_qos_constraints *c,
-					    unsigned long *cpus,
-					    unsigned long new_cpus,
-					    enum pm_qos_req_action new_action)
+					    unsigned long *cpus)
 {
+	s32 qos_val[NR_CPUS] = {
+		[0 ... (NR_CPUS - 1)] = PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE
+	};
 	struct pm_qos_request *req;
 	unsigned long new_req_cpus;
+	bool changed = false;
 	int cpu;
 
 	/*
@@ -286,49 +288,36 @@ static inline int pm_qos_set_value_for_cpus(struct pm_qos_request *new_req,
 	if (c != pm_qos_array[PM_QOS_CPU_DMA_LATENCY]->constraints)
 		return -EINVAL;
 
-	if (new_cpus) {
-		/* cpus_affine changed, so the old CPUs need to be refreshed */
-		new_req_cpus = new_req->cpus_affine | new_cpus;
-		new_req->cpus_affine = new_cpus;
-	} else {
-		new_req_cpus = new_req->cpus_affine;
-	}
-
-	if (new_action != PM_QOS_REMOVE_REQ) {
-		bool changed = false;
-
-		for_each_cpu(cpu, to_cpumask(&new_req_cpus)) {
-			if (c->target_per_cpu[cpu] != new_req->node.prio) {
-				changed = true;
-				break;
-			}
+	new_req_cpus = atomic_read(&new_req->cpus_affine);
+	for_each_cpu(cpu, to_cpumask(&new_req_cpus)) {
+		if (c->target_per_cpu[cpu] != new_req->node.prio) {
+			changed = true;
+			break;
 		}
 
 		if (!changed)
 			return 0;
 	}
 
+	if (!changed)
+		return 0;
+
 	plist_for_each_entry(req, &c->list, node) {
 		unsigned long affected_cpus;
 
-		affected_cpus = req->cpus_affine & new_req_cpus;
+		affected_cpus = atomic_read(&req->cpus_affine) & new_req_cpus;
 		if (!affected_cpus)
 			continue;
 
 		for_each_cpu(cpu, to_cpumask(&affected_cpus)) {
-			if (c->target_per_cpu[cpu] != req->node.prio) {
-				c->target_per_cpu[cpu] = req->node.prio;
-				*cpus |= BIT(cpu);
-			}
+			if (qos_val[cpu] > req->node.prio)
+				qos_val[cpu] = req->node.prio;
 		}
-
-		if (!(new_req_cpus &= ~affected_cpus))
-			return 0;
 	}
 
 	for_each_cpu(cpu, to_cpumask(&new_req_cpus)) {
-		if (c->target_per_cpu[cpu] != PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE) {
-			c->target_per_cpu[cpu] = PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE;
+		if (c->target_per_cpu[cpu] != qos_val[cpu]) {
+			c->target_per_cpu[cpu] = qos_val[cpu];
 			*cpus |= BIT(cpu);
 		}
 	}
@@ -375,7 +364,7 @@ static int pm_qos_update_target_cpus(struct pm_qos_constraints *c,
 
 	curr_value = pm_qos_get_value(c);
 	pm_qos_set_value(c, curr_value);
-	ret = pm_qos_set_value_for_cpus(req, c, &cpus, new_cpus, action);
+	ret = pm_qos_set_value_for_cpus(req, c, &cpus);
 
 	spin_unlock(&pm_qos_lock);
 
